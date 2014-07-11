783eb7c3909458fd4e4f18ecd5b15a5e4926b98f
Nathan Eloe <powerofazure@gmail.com>
2014-07-11 13:23:11 -0500

    add iostream

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 // this should do something
+#include <iostream>
+using namespace std;
 
 int main()
 {
 //chase, umm, do someting here
 cout << "hello world" << endl;
+cout << "goodbye world" << endl;
 return 0;
 }
